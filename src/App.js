5100fc51036284ce4d798de05ef63544fe889c53
Rakibul md <rakib245875@gmail.com>
2022-05-22 20:44:40 +0600

    Added: Favicon updated, title updated & some dependency added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <ToastContainer
+                position="top-center"
+                autoClose={3500}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+            ;
+        </div>
+    );
 }
 
 export default App;
